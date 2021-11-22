e37d081b55dcff5ea8c3fe639cc7edae8608fe1c
Jaykoo <jaykoo.9278@gmail.com>
2021-11-22 21:19:14 +0800

    .

---
+++ 算法/LeetCodes/0024.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (head === null || head.next === null) return head;
+    var node = head.next;
+    head.next = swapPairs(node.next);
+    node.next = head; 
+
+    return node
+};
